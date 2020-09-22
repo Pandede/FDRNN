Fix the propagating problem, replaced argmax with gumbel softmax

main.py
@@ -14,51 +14,60 @@ c = 0.01
 lag = 50
 fuzzy_degree = 3
 
+# Dataset
 dataset = IndexDataset('./Data', lag)
 dataloader = DataLoader(dataset, shuffle=False, batch_size=1)
-kmeans = KMeans(3)
 
+# Models
+kmeans = KMeans(3)
 fuzzy_layers = FuzzyLayer()
 fddr = FDDR(lag)
+
+# Tools
 optimizer = torch.optim.Adam(fddr.parameters())
 reward_meter = AverageMeter(100, len(dataloader))
 
+# Training Phase
 for e in range(10):
     with tqdm(total=len(dataloader)) as progress_bar:
         for i, (returns, fragments) in enumerate(dataloader):
             # Clustering with K-Means, computing the mean and variance of each group
-            o = torch.zeros(*fragments.size(), fuzzy_degree)
+            o = torch.zeros(*fragments.size(), fuzzy_degree, requires_grad=False)
             for f in range(fragments.size(1)):
                 fragment = fragments[:, f].T
                 label = kmeans.fit(fragment).labels_
-                mean = [torch.mean(fragment[label == k]) for k in range(fuzzy_degree)]
-                var = [torch.var(fragment[label == k]) for k in range(fuzzy_degree)]
+
+                mean = torch.zeros(3, requires_grad=False)
+                var = torch.zeros(3, requires_grad=False)
+                for k in range(fuzzy_degree):
+                    group = fragment[label == k]
+                    mean[k] = torch.mean(group) if len(group) > 0 else 0
+                    var[k] = torch.var(group) if len(group) > 1 else 0
 
                 # Propagating through fuzzy operation
                 o[:, f] = fuzzy_layers(fragment, mean, var)
             o = torch.flatten(o, -2)
 
             # Computing actions by using FDDR
-            delta_prob = fddr(o)
-            _, delta = torch.max(delta_prob, -1)
-            delta = delta.double() - 1
+            delta = fddr(o).double().squeeze(-1)
 
             # Computing reward
             pad_delta = F.pad(delta, [1, 0])
             delta_diff = (pad_delta[:, 1:] - pad_delta[:, :-1])
             reward = torch.sum(delta * returns - c * torch.abs(delta_diff))
-            reward = reward.requires_grad_()
 
             # Updating FDDR
             optimizer.zero_grad()
             (-reward).backward()
             optimizer.step()
 
+            # Recording and showing the information
             reward_meter.append(reward.item())
             progress_bar.set_description(
                 '[Epoch %d][Iteration %d][Reward: %.4f]' % (e, i, reward_meter.get_average(-1)))
             progress_bar.update()
         reward_meter.step()
 
+# Plot the reward curve
 plt.plot(reward_meter.get_average())
 plt.show()

model.py
@@ -1,13 +1,14 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class FDDR(nn.Module):
     def __init__(self, lag, fuzzy_degree=3):
         super(FDDR, self).__init__()
 
-        self.autoencoder = AutoEncoder(lag * fuzzy_degree, 3)
-        self.rnn = SequentialLayer(150)
+        self.autoencoder = AutoEncoder(lag * fuzzy_degree, 10)
+        self.rnn = SequentialLayer(10)
 
     def forward(self, x):
         h = self.autoencoder(x)
@@ -48,7 +49,11 @@ class SequentialLayer(nn.Module):
     def __init__(self, n_features):
         super(SequentialLayer, self).__init__()
         self.rnn = nn.RNN(n_features, 3, 1)
+        signal = torch.tensor([[-1.], [0.], [1.]])
+        self.register_buffer('signal', signal)
 
     def forward(self, x):
         output, hidden_state = self.rnn(x)
-        return torch.softmax(output, -1), hidden_state
+        output = F.gumbel_softmax(output, hard=True, dim=-1)
+        delta = torch.matmul(output, self.signal)
+        return delta, hidden_state