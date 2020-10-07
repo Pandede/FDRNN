1d4aa02b12e79626d8b4874655af925e6dcac87d
Pandede <steven9609@gmail.com>
2020-10-07 15:15:50 +0800

    Evaluate validation reward

--- main.py
+++ main.py
@@ -21,20 +21,24 @@ fuzzy_degree = 3
 # streamer.transform('./Data/futures/train', './Data/fuzzy_futures/train')
 
 # Dataset
-dataset = FuzzyIndexDataset('./Data/fuzzy_futures/train', lag)
-dataloader = DataLoader(dataset, shuffle=False, batch_size=1)
+train_dataset = FuzzyIndexDataset('./Data/fuzzy_futures/train', lag)
+train_dataloader = DataLoader(train_dataset, shuffle=False, batch_size=1)
+test_dataset = FuzzyIndexDataset('./Data/fuzzy_futures/test', lag)
+test_dataloader = DataLoader(test_dataset, shuffle=False, batch_size=1)
 
 # Models
 fddr = FDDR(lag, fuzzy_degree)
 
 # Tools
 optimizer = torch.optim.Adam(fddr.parameters())
-reward_meter = AverageMeter(epochs, len(dataloader))
+train_reward_meter = AverageMeter(epochs, len(train_dataloader))
+test_reward_meter = AverageMeter(epochs, len(test_dataloader))
 
 # Training Phase
 for e in range(epochs):
-    with tqdm(total=len(dataloader)) as progress_bar:
-        for i, (returns, fragments, mean, var) in enumerate(dataloader):
+    with tqdm(total=len(train_dataloader), ncols=130) as progress_bar:
+        fddr.train()
+        for i, (returns, fragments, mean, var) in enumerate(train_dataloader):
             # Computing actions by using FDDR
             delta = fddr(fragments, running_mean=mean, running_var=var).double().squeeze(-1)
 
@@ -49,19 +53,39 @@ for e in range(epochs):
             optimizer.step()
 
             # Recording and showing the information
-            reward_meter.append(reward.item())
+            train_reward_meter.append(reward.item())
             progress_bar.set_description(
-                '[Epoch %d][Iteration %d][Reward: %.4f]' % (e, i, reward_meter.get_average(-1)))
+                '[Epoch %d][Iteration %d][Reward: train = %.4f]' %
+                (e, i, train_reward_meter.get_average(-1)))
             progress_bar.update()
+
+        fddr.eval()
+        with torch.no_grad():
+            for i, (returns, fragments, mean, var) in enumerate(test_dataloader):
+                # Computing actions by using FDDR
+                delta = fddr(fragments, running_mean=mean, running_var=var).double().squeeze(-1)
+
+                # Computing reward
+                pad_delta = F.pad(delta, [1, 0])
+                delta_diff = (pad_delta[:, 1:] - pad_delta[:, :-1])
+                reward = torch.sum(delta * returns - c * torch.abs(delta_diff))
+
+                test_reward_meter.append(reward.item())
+
+        progress_bar.set_description(
+            '[Epoch %d][Iteration %d][Reward: train = %.4f, test = %.4f]' %
+            (e, i, train_reward_meter.get_average(-1), test_reward_meter.get_average(-1)))
 
         if e % save_per_epoch == 0:
             torch.save(fddr.state_dict(), './Pickle/fddrl.pkl')
-        reward_meter.step()
+        train_reward_meter.step()
+        test_reward_meter.step()
 
 # Save the model and reward history
 torch.save(fddr.state_dict(), './Pickle/fddrl.pkl')
-np.save('./Pickle/fddrl_reward.npy', reward_meter.get_average())
+np.save('./Pickle/fddrl_reward.npy', train_reward_meter.get_average())
 
 # Plot the reward curve
-plt.plot(reward_meter.get_average())
+plt.plot(train_reward_meter.get_average())
+plt.plot(test_reward_meter.get_average())
 plt.show()
